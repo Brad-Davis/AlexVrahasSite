78e4f84ff4ff5d8446a9cd2dba52c2c1b49c93e5
Brad-Davis <bcd296@nyu.edu>
2020-09-18 00:16:24 -0400

    guts of the site working

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 
-const linkArray = ['/musicvideos.html', '/productvideos.html', 'personalprojects.html'];
-
-
-
-console.log('working!');
+const linkArray = ['musicvideos.html', 'productvideos.html', 'personalprojects.html'];
 
 function setVideoLinks(content, num){
     content.children[1].src = "images/gifs/explosion.gif"
     setTimeout(function(){
         window.location.href = linkArray[num];
     }, 1000);
+}
+
+function setHomeLinks(content, link, gif){
+    if(gif){
+        content.children[0].src = gif
+    } else {
+        content.children[0].src = "images/gifs/explosion.gif"
+    }
+    
+    setTimeout(function(){
+        window.location.href = link;
+    }, 1000);
+}
+
+function dontTouchMe(){
+    console.log("working")
+    const alex = document.getElementById('angryAlex');
+    alex.style.display = "block";
+    alex.style.opacity = "1"
+    alex.style.zIndex = "6"
+
+    $('#angryAlex').fadeOut(2000, function(){
+        alex.zIndex = "-3"
+        console.log("poo");
+    })
+    
+}
+
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
 }
