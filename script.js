b6ef06c563227f74b72d817a1e7a19ceccc9767c
Brad Davis <braddavis97@gmail.com>
2020-10-16 14:32:06 -0400

    damn this is looking good!

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 
 const linkArray = ['musicvideos.html', 'productvideos.html', 'narrative.html'];
 
-function setVideoLinks(content, num){
+function setVideoLinks(content, num) {
     content.children[1].src = "images/gifs/explosion.gif"
-    setTimeout(function(){
+    setTimeout(function () {
         window.location.href = linkArray[num];
     }, 1000);
 }
 
-function setHomeLinks(content, link, gif){
-    if(gif){
+function setHomeLinks(content, link, gif) {
+    if (gif) {
         content.children[0].src = gif
     } else {
         content.children[0].src = "images/gifs/explosion.gif"
     }
-    
-    setTimeout(function(){
+
+    setTimeout(function () {
         window.location.href = link;
     }, 1000);
 }
 
-function dontTouchMe(){
+function dontTouchMe() {
     console.log("working")
     const alex = document.getElementById('angryAlex');
-    alex.style.display = "block";
     alex.style.opacity = "1"
     alex.style.zIndex = "6"
+    $('#angryAlex').show();
 
-    $('#angryAlex').fadeOut(2000, function(){
+    $('#angryAlex').fadeOut(2000, function () {
         alex.zIndex = "-3"
         console.log("poo");
     })
-    
+
 }
 
 function openInNewTab(url) {
